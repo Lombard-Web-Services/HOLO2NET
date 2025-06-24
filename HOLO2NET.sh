5d24d50935783e7c0bd9b4745656050c2f9ac9c7
Thibaut Lombard <64758434+Lombard-Web-Services@users.noreply.github.com>
2025-06-24 15:19:57 +0200

    Update

--- HOLO2NET.sh
+++ HOLO2NET.sh
@@ -2,9 +2,11 @@
 file=$1
 echo "translating $file to hologram.mp4";
 backgroundremover -i $file -tv -o "v.mp4"
-ffmpeg -i v.mp4 -vf "rotate=45*(PI/180):bilinear=0" vTL.mp4
-ffmpeg -i vTL.mp4 -vf vflip vTR.mp4
-ffmpeg -i vTL.mp4 -vf hflip vBL.mp4
-ffmpeg -i vTL -vf "rotate=225*(PI/180):bilinear=0" vBR.mp4
-ffmpeg -i vTL.mp4 -i vTR.mp4 -i vBL.mp4 -i vBR.mp4 -filter_complex "[0:v][1:v][2:v][3:v]xstack=inputs=4:layout=0_0|w0_0|0_h0|w0_h0[v]" -map "[v]" hologram.mp4
+ffmpeg -i v.mp4 -filter_complex "\
+[0:v]rotate=45*PI/180:bilinear=0,split=3[vTL1][vTL2][vTL3]; \
+[vTL1]vflip[vTR]; \
+[vTL2]hflip[vBL]; \
+[0:v]rotate=315*PI/180:bilinear=0,hflip[vBR]; \
+[vTL3][vTR][vBL][vBR]xstack=inputs=4:layout=0_0|w0_0|0_h0|w0_h0,fps=22[outv]" \
+-map "[outv]" -map 0:a? -c:v libx264 -pix_fmt yuv420p -c:a copy hologram.mp4
 echo "Script finished !";
